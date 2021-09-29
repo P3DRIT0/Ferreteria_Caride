240bed526da27d7bcfe9c426b4598c720fa10ac7
P3DRIT0 <78014295+P3DRIT0@users.noreply.github.com>
2021-09-29 19:12:55 +0200

    Página Principal de la ferretería
    Se ha añadido la página web de la Ferretería esta incluye Fullpage.js  además de boostrap

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const seccionesPagina = new fullpage('#fullpage', {
+	// ──────────────────────────────────────────────────
+	//   :::::: Opciones Basicas
+	// ──────────────────────────────────────────────────
+	autoScrolling: true, // Se activa el scroll.
+	fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+	fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+	easing: 'easeOutBack', // Funcion de tiempo de la animacion.
+	scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
+	css3: true, // Si usara CSS3 o javascript.
+	easingcss3: 'cubic-bezier(0.34, 1.56, 0.64, 1)', // Curva de velocidad del efecto.
+	loopBottom: false,
+	// ──────────────────────────────────────────────────
+	//   :::::: Barra de navegación
+	// ──────────────────────────────────────────────────
+	navigation: true, // Muesta la barra de navegación.
+	menu: '#menu', // Menu de navegación.
+	anchors: ['inicio', 'productos','novedades', 'contacto',], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+	navigationTooltips: ['Inicio', 'Productos','Novedades','Contacto'], // Tooltips que mostrara por cada boton.
+	showActiveTooltip: false, // Mostrar tooltip activa.
+	// ──────────────────────────────────────────────────
+	//   :::::: Secciones
+	// ──────────────────────────────────────────────────
+	sectionsColor : ['#000', '#c2c2c2', '#000'], // Color de fondo de cada seccion.
+	verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
+	// ──────────────────────────────────────────────────
+	//   :::::: Slides
+	// ──────────────────────────────────────────────────
+	controlArrows: true, // Flechas del slide
+	slidesNavigation: false, // Indicadores del slide
+	afterLoad: function(origin, destination){
+		if(destination.anchor == 'contacto'){
+			document.querySelector('.footer h2').style.opacity = 1;
+		}
+	}
+});
